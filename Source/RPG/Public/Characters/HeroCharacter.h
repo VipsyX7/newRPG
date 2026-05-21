9439c968c898d5eb6e76dad5d647d463c38a8574
VipsyX7 <1693725065a@gmail.com>
2026-05-21 14:13:27 +0800

    first

---
+++ Source/RPG/Public/Characters/HeroCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/RPGBaseCharacters.h"
+#include "HeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API AHeroCharacter : public ARPGBaseCharacters
+{
+	GENERATED_BODY()
+	
+};
